605b99262927739a7e61f272976ae2e363c47488
Uma Maheshwar <karnakar231j@gmail.com>
2026-01-12 11:32:38 +0530

    Create main.ts

---
+++ src/main.ts
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>Simple Task Manager</title>
+  <base href="/">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+  <!-- Root component -->
+  <!-- Simple setup – easy ga HR ki run avvali -->
+  <app-root></app-root>
+</body>
+</html>
